e4b64f8750b264303184ea72efb3ea2eb5f919fa
dragosbus <dragosbusuioc94@gmail.com>
2018-08-27 14:46:34 +0200

    add default image if not exist one

--- src/components/Carousel/Slide.js
+++ src/components/Carousel/Slide.js
@@ -4,13 +4,14 @@ const Slide = props => {
   let article = props.article;
   let srcImg = article
     ? article.urlToImage
-    : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1024px-No_image_3x4.svg.png';
+    : '';
+  let imgNull = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1024px-No_image_3x4.svg.png';
   let titleImg = article ? article.title : '';
   let href = article ? article.url : '';
 
   return (
     <div className="carousel-slide">
-      <img src={srcImg} alt={titleImg} />
+      <img src={srcImg || imgNull} alt={titleImg} />
       <a className="carousel-slide-title" href={href}>
         {titleImg}
       </a>
--- src/components/News/NewsElement.js
+++ src/components/News/NewsElement.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const ElementNews = props => {
-  return(
+  let imgNull =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/No_image_3x4.svg/1024px-No_image_3x4.svg.png';
+  let titleImg = props.article ? props.article.title : '';
+  return (
     <li className="news">
-      <img src={props.urlToImage}/>
+      <img src={props.urlToImage || imgNull} alt={titleImg} />
       <p>{props.title}</p>
     </li>
   );
