c0073ef52b3587db130b18f1915695dc4dc32a03
dragosbus <dragosbusuioc94@gmail.com>
2018-08-27 19:16:29 +0200

    get a new page for the news cliked

--- src/components/FullNews/FullNews.js
+++ src/components/FullNews/FullNews.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import store from '../../store';
 
-
-const FullNews = (props) => {
-    console.log(props)
-    return(
-        <div>
-            {props.match.params.id}
-        </div>
-    );
-}
+const FullNews = props => {
+  console.log(props);
+  let data = store.getState().topUS;
+  return <div>{data[props.match.params.id].title}</div>;
+};
 
 export default FullNews;
